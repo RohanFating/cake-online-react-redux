1d99a72d93eac925276ddd48af2f717887dff188
Rohan Fating <gaurirohan@Gauris-MacBook-Pro.local>
2018-04-09 13:13:34 +0100

    (feat): Done implementation of cake info components, cake-details component, add-cake components. Added router to navigate between the pages Files Modified add cake components js and css, cake-info component js and css, cake-details component js and css, reducer Features Remaining: Unit testing

--- cake-app/src/App.js
+++ cake-app/src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { connect, Provider } from 'react-redux';
 import './App.css';
+import CakeInfoComponent from './components/cake-info/cake-info.component.js';
+import AddCakeComponent from './components/add-cake/add-cake.component.js';
+import CakeDetailComponent from './components/cake-details/cake-detail.component.js';
+import AppActions from './actions/app.actions.js';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 class App extends Component {
+
+  /**
+   * To dispatch event 
+   * @param event - name of event 
+   * @param data - data respective to the event event
+   */
+  updateCakeInfoList(event){
+    AppActions.processAction( event, this.props.store.dispatch );
+  }
+
   render() {
     return (
       <div className="container">
@@ -10,9 +29,36 @@ class App extends Component {
              Cake Links Online Store
           </div>
         </div>
+        <div className="row">
+        <Provider store={this.props.store}>
+          <Router>
+            <div className="col-md-12">
+              <Route exact path="/" render={()=> {
+                return <CakeInfoComponent 
+                updateCakeList ={(event, data)=> { return this.updateCakeInfoList(event)}} 
+                cakeInfo={ this.props.cakeInfo }/> }} />
+              <Route path="/cake-detail/:id" render={(match)=> {
+                return <CakeDetailComponent match={match}
+                cakeDetails={ this.props.cakeDetails }
+                 updateCakeList ={(event, data)=> { return this.updateCakeInfoList(event, data)}}/> }} />
+              <Route path="/add-cake" render={()=> {
+                return <AddCakeComponent cakeFormDetails={ this.props.cakeFormDetails }  
+                updateCakeList ={(event, data)=> { return this.updateCakeInfoList(event, data)}} /> }}/>
+            </div>
+          </Router>
+          </Provider>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    cakeInfo: state.cakeInfo,
+    cakeFormDetails: state.cakeFormDetails,
+    cakeDetails: state.cakeDetails,
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- cake-app/src/index.js
+++ cake-app/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/app.store.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import './components/cake-info/cake-info.component.css';
+import './components/add-cake/add-cake.component.css';
+import './components/cake-details/cake-detail.component.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
